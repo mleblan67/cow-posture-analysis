a05c4b2b0ee896eac965ba2d096be0c0645f9a7f
Max <mleblang@wisc.edu>
2024-02-16 17:54:04 -0600

    UWB and Accelerometer data

--- pooling_exp.py
+++ pooling_exp.py
@@ -6,6 +6,7 @@ from numpy import std
 from numpy import asarray
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
+from pandas import merge
 
 from utils.load_data_utils import load_to_df, create_rolling_window_data
 from utils.features_utils import add_svm_feature
@@ -75,154 +76,15 @@ def summarize_results(scores):
 
     return m
 
+def run_pooling_on_single_tag_single_day(repeats=3):
+    accel_data_prefix = 'converted_data/'
+    uwb_data_prefix = 'location_data/'
 
-def run_total_pooling_experiment(prefix='converted_data/', repeats=10):
-    # The tag numbers we want to test on
-    tags = range(1,11)
-    # Array of all the data we load in from each tag
-    inputs = []
-    groundtruths = []
-
-    # Load in all the data
-    for tag in tags:
-        # Build folder path
-        data_dir = prefix + 'T' + str(tag).zfill(2) + '/'
-
-        # Get all sensor data files for this folder
-        filepaths = os.listdir(data_dir)
-        filepaths = [data_dir + file for file in filepaths if file.startswith('sensor_data') and file.endswith('.csv')]
-        filepaths.sort() # Make sure they're in order for processing
-
-        # Get groundtruth path
-        groundtruth_path = data_dir + 'T' + str(tag).zfill(2) + '_groundtruths.csv'
-
-        # Load in the data
-        input_df, groundtruth_df = load_to_df(filepaths, groundtruth_path)
-
-        # Add to array
-        inputs.append(input_df)
-        groundtruths.append(groundtruth_df)
-
-        print("Loaded Tag " + str(tag))
-
-    # Create X and y window data
-    Xs = []
-    ys = []
-
-    for tag, input_df, groundtruth_df in zip(tags, inputs, groundtruths):
-        # Create rolling window
-        X, y = create_rolling_window_data(input_df, groundtruth_df)
-
-        # Add to data arrays to combine all data
-        Xs += X
-        ys += y
-
-        print("Created sliding window for Tag " + str(tag))
-
-    ys = to_categorical(ys)
-    Xs = asarray(Xs)
-
-    # Train/Test split for data
-    X_train, X_test, y_train, y_test = train_test_split(Xs, ys, test_size=0.30)
-    print("Training data shape: (X) (y)")
-    print(X_train.shape, y_train.shape)
-    print("Testing data shape: (X) (y)")
-    print(X_test.shape, y_test.shape)
-
-    print('Data loaded! Ready to train')
-
-    # repeat experiment
-    scores = list()
-    for r in range(repeats):
-        score = build_model(X_train, y_train, X_test, y_test)
-        score = score * 100.0
-        print('>#%d: %.3f' % (r+1, score))
-        scores.append(score)
-    # summarize results
-    summarize_results(scores)
-
-def run_single_day_pooling_experiment(prefix='converted_data/', repeats=10):
-    # The tag numbers we want to test on
-    tags = range(1,11)
-    # Array of all the data we load in from each tag
-    inputs = []
-    groundtruths = []
-
-    # Load in all the data
-    for tag in tags:
-        # Build folder path
-        data_dir = prefix + 'T' + str(tag).zfill(2) + '/'
-
-        # Get all sensor data files for this folder
-        # filepaths = os.listdir(data_dir)
-        # filepaths = [data_dir + file for file in filepaths if file.startswith('sensor_data') and file.endswith('.csv')]
-        # filepaths.sort() # Make sure they're in order for processing
-        filepaths = [data_dir + 'sensor_data_T' + str(tag).zfill(2)+'_0725.csv']
-
-        # Get groundtruth path
-        groundtruth_path = data_dir + 'T' + str(tag).zfill(2) + '_groundtruths.csv'
-
-        # Load in the data
-        input_df, groundtruth_df = load_to_df(filepaths, groundtruth_path)
-
-        # Add to array
-        inputs.append(input_df)
-        groundtruths.append(groundtruth_df)
-
-        print("Loaded Tag " + str(tag))
-
-
-
-    test_input = inputs.pop(5)
-    test_groundtruth = groundtruths.pop()
-
-    X_test, y_test = create_rolling_window_data(test_input, test_groundtruth)
-    y_test = to_categorical(y_test)
-    X_test = asarray(X_test)
-
-
-
-    # Create X and y window data
-    X_train = []
-    y_train = []
-
-    for tag, input_df, groundtruth_df in zip(tags, inputs, groundtruths):
-        # Create rolling window
-        X, y = create_rolling_window_data(input_df, groundtruth_df)
-
-        # Add to data arrays to combine all data
-        X_train += X
-        y_train += y
-
-        print("Created sliding window for Tag " + str(tag))
-
-    y_train = to_categorical(y_train)
-    X_train = asarray(X_train)
-
-    # Train/Test split for data
-    print("Training data shape: (X) (y)")
-    print(X_train.shape, y_train.shape)
-    print("Testing data shape: (X) (y)")
-    print(X_test.shape, y_test.shape)
-
-    print('Data loaded! Ready to train')
-
-    # repeat experiment
-    scores = list()
-    for r in range(repeats):
-        score = build_model(X_train, y_train, X_test, y_test)
-        score = score * 100.0
-        print('>#%d: %.3f' % (r+1, score))
-        scores.append(score)
-    # summarize results
-    summarize_results(scores)
-
-def run_pooling_on_single_tag_single_day(prefix='converted_data/', repeats=10, sensor='sensor_data'):
     # The tag numbers we want to train on
     train_tags = [1,2,3,4,5,6,7,8,9,10]
     # The tag numbers we want to test on
-    # test_tags = [1,2,3,4,5,6,7,8,9,10]
-    test_tags = [7]
+    test_tags = [1,2,3,4,5,6,7,8,9,10]
+    # test_tags = [7]
 
     # Array of all the training data we load in from each tag
     train_inputs = []
@@ -235,20 +97,30 @@ def run_pooling_on_single_tag_single_day(prefix='converted_data/', repeats=10, s
     # Load in all the training
     for tag in train_tags:
         # Build folder path
-        data_dir = prefix + 'T' + str(tag).zfill(2) + '/'
+        accel_data_dir = accel_data_prefix + 'T' + str(tag).zfill(2) + '/'
+        uwb_data_dir = uwb_data_prefix + 'T' + str(tag).zfill(2) + '/'
         groundtruth_dir = 'converted_data/' + 'T' + str(tag).zfill(2) + '/'
 
-        # Get all sensor data files for this folder
-        filepaths = os.listdir(data_dir)
-        filepaths = [data_dir + file for file in filepaths if file.startswith(sensor) and file.endswith('.csv')]
-        filepaths.sort() # Make sure they're in order for processing
-        
+        # Get all accelerometer sensor data files for this folder
+        accel_filepaths = os.listdir(accel_data_dir)
+        accel_filepaths = [accel_data_dir + file for file in accel_filepaths if file.startswith('sensor_data') and file.endswith('.csv')]
+        accel_filepaths.sort() # Make sure they're in order for processing
 
+        # Get all UWB sensor data files for this folder
+        uwb_filepaths = os.listdir(uwb_data_dir)
+        uwb_filepaths = [uwb_data_dir + file for file in uwb_filepaths if file.startswith('uwb_loc') and file.endswith('.csv')]
+        uwb_filepaths.sort() # Make sure they're in order for processing
+        
         # Get groundtruth path
         groundtruth_path = groundtruth_dir + 'T' + str(tag).zfill(2) + '_groundtruths.csv'
 
-        # Load in the data
-        input_df, groundtruth_df = load_to_df(filepaths, groundtruth_path)
+        # Load in both sensor data
+        accel_input_df, groundtruth_df = load_to_df(accel_filepaths, groundtruth_path)
+        uwb_input_df, _ = load_to_df(uwb_filepaths, groundtruth_path)
+
+        # Combine all sensor data together
+        input_df = merge(accel_input_df, uwb_input_df, how='outer', on='timestamp')
+
         print(f"Loaded in tag {tag}")
         # Create sliding window
         X, y = create_rolling_window_data(input_df, groundtruth_df)
@@ -261,29 +133,38 @@ def run_pooling_on_single_tag_single_day(prefix='converted_data/', repeats=10, s
     # Load in all the testing (just one day)
     for tag in test_tags:
         # Build folder path
-        data_dir = prefix + 'T' + str(tag).zfill(2) + '/'
+        accel_data_dir = accel_data_prefix + 'T' + str(tag).zfill(2) + '/'
+        uwb_data_dir = uwb_data_prefix + 'T' + str(tag).zfill(2) + '/'
         groundtruth_dir = 'converted_data/' + 'T' + str(tag).zfill(2) + '/'
 
-        # Get all sensor data files for this folder
-        filepaths = os.listdir(data_dir)
-        filepaths = [data_dir + file for file in filepaths if file.startswith(sensor) and file.endswith('0725.csv')]
-        filepaths.sort() # Make sure they're in order for processing
-        
+        # Get all accelerometer sensor data files for this folder
+        accel_filepaths = os.listdir(accel_data_dir)
+        accel_filepaths = [accel_data_dir + file for file in accel_filepaths if file.startswith('sensor_data') and file.endswith('.csv')]
+        accel_filepaths.sort() # Make sure they're in order for processing
 
+        # Get all UWB sensor data files for this folder
+        uwb_filepaths = os.listdir(uwb_data_dir)
+        uwb_filepaths = [uwb_data_dir + file for file in uwb_filepaths if file.startswith('uwb_loc') and file.endswith('.csv')]
+        uwb_filepaths.sort() # Make sure they're in order for processing
+        
         # Get groundtruth path
         groundtruth_path = groundtruth_dir + 'T' + str(tag).zfill(2) + '_groundtruths.csv'
 
-        # Load in the data
-        input_df, groundtruth_df = load_to_df(filepaths, groundtruth_path)
+        # Load in both sensor data
+        accel_input_df, groundtruth_df = load_to_df(accel_filepaths, groundtruth_path)
+        uwb_input_df, _ = load_to_df(uwb_filepaths, groundtruth_path)
+
+        # Combine all sensor data together
+        input_df = merge(accel_input_df, uwb_input_df, how='outer', on='timestamp')
+
         print(f"Loaded in tag {tag}")
         # Create sliding window
         X, y = create_rolling_window_data(input_df, groundtruth_df)
         print(f"Created Sliding window for tag {tag} \n")
 
         # Add to array
-        test_inputs.append(X)
-        test_groundtruths.append(y)
-
+        train_inputs.append(X)
+        train_groundtruths.append(y)
 
 
     accuracies = []
@@ -315,8 +196,6 @@ def run_pooling_on_single_tag_single_day(prefix='converted_data/', repeats=10, s
         y_test = to_categorical(y_test)
         X_test = asarray(X_test)
 
-
-
         # Train/Test split for data
         print("Training data shape: (X) (y)")
         print(X_train.shape, y_train.shape)
@@ -326,22 +205,21 @@ def run_pooling_on_single_tag_single_day(prefix='converted_data/', repeats=10, s
         print('Data loaded! Ready to train')
 
         # graph exp
-        graph_model(X_train, y_train, X_test, y_test)
+        # graph_model(X_train, y_train, X_test, y_test)
 
         # repeat experiment
-    #     scores = list()
-    #     for r in range(repeats):
-    #         score = build_model(X_train, y_train, X_test, y_test)
-    #         score = score * 100.0
-    #         print('>#%d: %.3f' % (r+1, score))
-    #         scores.append(score)
-    #     # summarize results
-    #     m = summarize_results(scores)
-    #     accuracies.append(m)
+        scores = list()
+        for r in range(repeats):
+            score = build_model(X_train, y_train, X_test, y_test)
+            score = score * 100.0
+            print('>#%d: %.3f' % (r+1, score))
+            scores.append(score)
+        # summarize results
+        m = summarize_results(scores)
+        accuracies.append(m)
 
     # print(f"OVERALL ACCURACY WAS {mean(accuracies)}")
 
     
 
-run_pooling_on_single_tag_single_day(prefix='location_data/',repeats=3,sensor='uwb_loc')
-# run_pooling_on_single_tag_single_day()
+run_pooling_on_single_tag_single_day()
