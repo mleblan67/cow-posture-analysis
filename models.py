28d3de816f6d62f1b89847e9b9581833cf5b5850
Max <mleblang@wisc.edu>
2024-05-01 16:39:24 -0500

    Add wavelet processing

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Conv1D, LSTM
-from tensorflow.keras.layers import MaxPooling1D
+from tensorflow.keras.layers import Conv1D, Conv2D
+from tensorflow.keras.layers import MaxPooling1D, MaxPooling2D
 
 
 '''
@@ -27,26 +27,19 @@ def CNN(n_timesteps, n_features, n_outputs):
 
     return model
 
-'''
-CNN-LSTM model
-Runs two convolutions on the data then
-passes the filtered data into an LSTM for time sensitive classification
-'''
-def CNN_LSTM(n_timesteps, n_features, n_outputs):
+def CNN2D(wv_x, wv_y, wv_channels, num_classes):
     model = Sequential()
-    model.add(Conv1D(filters=32, kernel_size=4, activation='relu',
-              input_shape=(n_timesteps, n_features)))
-    # model.add(Dropout(0.2))
-    # model.add(MaxPooling1D(pool_size=2))
-    model.add(Conv1D(filters=64, kernel_size=4, activation='relu'))
-    model.add(Dropout(0.2))
-    model.add(MaxPooling1D(pool_size=2))
-
-    model.add(LSTM(4, dropout=0.2))
+    model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
+                    activation='relu',
+                    input_shape=(wv_x, wv_y, wv_channels)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(n_outputs, activation='softmax'))
+    model.add(Dense(1000, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
-    
+
     return model
--- raw_behavior_exp.py
+++ raw_behavior_exp.py
@@ -12,11 +12,12 @@ from pandas import merge
 import pandas as pd
 import datetime
 import numpy as np
+import pywt
 import seaborn as sns
 
 from utils.load_data_utils import load_to_df, create_rolling_window_data
 from utils.features_utils import add_svm_feature
-from models import CNN, CNN_LSTM
+from models import CNN, CNN2D
 from tensorflow.keras.utils import to_categorical
 
 
@@ -31,10 +32,9 @@ session = InteractiveSession(config=config)
 # Train and evaluate a model
 def build_model(trainX, trainy, testX, testy):
     verbose, epochs, batch_size = 0, 15, 32
-    n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
-    n_batches = trainX.shape[0]
+    wv_x, wv_y, wv_channels, num_classes = trainX.shape[1], trainX.shape[2], trainX.shape[3], trainy.shape[1]
 
-    model = CNN(n_timesteps, n_features, n_outputs)
+    model = CNN2D(wv_x, wv_y, wv_channels, num_classes)
     # fit network
     model.fit(trainX, trainy, epochs=epochs,
               batch_size=batch_size, verbose=verbose)
@@ -136,7 +136,7 @@ def run_exp(repeats=3):
 
         # Get all accelerometer sensor data files for this folder
         accel_filepaths = os.listdir(accel_data_dir)
-        accel_filepaths = [accel_data_dir + file for file in accel_filepaths if file.startswith('sensor_data') and file.endswith('.csv')]
+        accel_filepaths = [accel_data_dir + file for file in accel_filepaths if file.startswith('sensor_data') and file.endswith('0725.csv')]
         accel_filepaths.sort() # Make sure they're in order for processing
 
         # Get all UWB sensor data files for this folder
@@ -270,4 +270,192 @@ def run_exp(repeats=3):
 
     print(f"OVERALL ACCURACY WAS {mean(accuracies)}")
 
-run_exp()
+
+
+def run_wv_exp(repeats=3):
+    accel_data_prefix = 'converted_data/'
+    uwb_data_prefix = 'location_data/'
+
+    # The tag numbers we want to train on
+    # train_tags = [1,2,3,4,5,6,8,9,10]
+    train_tags = [3,5,8,9,10]
+    # The tag numbers we want to test on
+    # test_tags = [1,2,3,4,5,6,7,8,9,10]
+    test_tags = [7]
+
+    # Array of all the training data we load in from each tag
+    train_inputs = []
+    train_groundtruths = []
+    # Array of all the testing data we load in from each tag
+    test_inputs = []
+    test_groundtruths = []
+
+    # Wavelet constants
+    waveletname = "mexh"
+    scales = range(1,201)
+
+
+    # Load in all the training
+    for tag in train_tags:
+        # Build folder path
+        accel_data_dir = accel_data_prefix + 'T' + str(tag).zfill(2) + '/'
+        uwb_data_dir = uwb_data_prefix + 'T' + str(tag).zfill(2) + '/'
+        groundtruth_dir = 'behavior_analysis/individual_behaviors/'
+
+        # Get all accelerometer sensor data files for this folder
+        accel_filepaths = os.listdir(accel_data_dir)
+        accel_filepaths = [accel_data_dir + file for file in accel_filepaths if file.startswith('sensor_data') and file.endswith('0725.csv')]
+        accel_filepaths.sort() # Make sure they're in order for processing
+
+        # Get all UWB sensor data files for this folder
+        # uwb_filepaths = os.listdir(uwb_data_dir)
+        # uwb_filepaths = [uwb_data_dir + file for file in uwb_filepaths if file.startswith('uwb_loc') and file.endswith('.csv')]
+        # uwb_filepaths.sort() # Make sure they're in order for processing
+        
+        # Get groundtruth path
+        groundtruth_path = groundtruth_dir + 'C' + str(tag).zfill(2) + '_0725.csv'
+
+        # Load in both sensor data
+        accel_input_df, groundtruth_df = load_to_df(accel_filepaths, groundtruth_path)
+        # uwb_input_df, groundtruth_df = load_to_df(uwb_filepaths, groundtruth_path)
+
+        # Combine all sensor data together
+        # input_df = merge(accel_input_df, uwb_input_df, how='outer', on='timestamp')
+
+        print(f"Loaded in tag {tag}")
+        # Create sliding window
+        X, y = create_rolling_window_data(accel_input_df, groundtruth_df,window_size=20, stride=10)
+        print(f"Created Sliding window for tag {tag} \n")
+
+         # Create wavelets
+        wvs = np.ndarray((len(X),200,200,3))
+
+        for wi in range(len(X)):
+            # Transpose into X, Y, and Z channels
+            window = X[wi].T
+            for si in range(3):
+                signal = window[si]
+                coeff, _ = pywt.cwt(signal, scales, waveletname, 1)
+                wvs[wi, :, :, si] = coeff
+
+        # Add to array
+        train_inputs.append(wvs)
+        train_groundtruths.append(y)
+
+        # Manage memory
+        del [accel_input_df, groundtruth_df]
+        gc.collect()
+
+
+    # Load in all the testing (just one day)
+    for tag in test_tags:
+        # Build folder path
+        accel_data_dir = accel_data_prefix + 'T' + str(tag).zfill(2) + '/'
+        uwb_data_dir = uwb_data_prefix + 'T' + str(tag).zfill(2) + '/'
+        groundtruth_dir = 'behavior_analysis/individual_behaviors/'
+
+        # Get all accelerometer sensor data files for this folder
+        accel_filepaths = os.listdir(accel_data_dir)
+        accel_filepaths = [accel_data_dir + file for file in accel_filepaths if file.startswith('sensor_data') and file.endswith('0725.csv')]
+        accel_filepaths.sort() # Make sure they're in order for processing
+
+        # Get all UWB sensor data files for this folder
+        # uwb_filepaths = os.listdir(uwb_data_dir)
+        # uwb_filepaths = [uwb_data_dir + file for file in uwb_filepaths if file.startswith('uwb_loc') and file.endswith('0725.csv')]
+        # uwb_filepaths.sort() # Make sure they're in order for processing
+        
+        # Get groundtruth path
+        groundtruth_path = groundtruth_dir + 'C' + str(tag).zfill(2) + '_0725.csv'
+
+        # Load in both sensor data
+        accel_input_df, groundtruth_df = load_to_df(accel_filepaths, groundtruth_path)
+        # uwb_input_df, groundtruth_df = load_to_df(uwb_filepaths, groundtruth_path)
+
+        # Combine all sensor data together
+        # input_df = merge(accel_input_df, uwb_input_df, how='outer', on='timestamp')
+
+        print(f"Loaded in tag {tag}")
+        # Create sliding window
+        X, y = create_rolling_window_data(accel_input_df, groundtruth_df,window_size=20,stride=10)
+        print(f"Created Sliding window for tag {tag} \n")
+
+        # Create wavelets
+        wvs = np.ndarray((len(X),200,200,3))
+
+        for wi in range(len(X)):
+            # Transpose into X, Y, and Z channels
+            window = X[wi].T
+            for si in range(3):
+                signal = window[si]
+                coeff, _ = pywt.cwt(signal, scales, waveletname, 1)
+                wvs[wi, :, :, si] = coeff
+
+        # Add to array
+        test_inputs.append(wvs)
+        test_groundtruths.append(y)
+
+        # Manage memory
+        del [accel_input_df, groundtruth_df]
+        gc.collect()
+
+
+    accuracies = []
+    # Loop through every test tag to train on all other data and test on this one
+    for test_tag_i in range(len(test_tags)):
+        print(f"Testing on tag {test_tags[test_tag_i]}")
+
+        # Prepare training data
+        # Combine all training data
+        X_train = np.array([])
+        y_train = np.array([])
+
+        for i in range(len(train_tags)):
+            # Skip if this is the tag we're testing on
+            if train_tags[i] == test_tags[test_tag_i]:
+                continue
+
+            # X_train += train_inputs[i]
+            # y_train += train_groundtruths[i]
+            if X_train.shape[0] == 0:
+                X_train = np.copy(train_inputs[i])
+            else:
+                X_train = np.vstack([X_train, train_inputs[i]])
+            
+            if y_train.shape[0] == 0:
+                y_train = np.copy(train_groundtruths[i])
+            else:
+                y_train = np.concatenate((y_train, train_groundtruths[i]))
+                
+
+        # One-hot encoding
+        y_train = to_categorical(y_train)
+
+        # Prepare testing data
+        X_test = test_inputs[test_tag_i]
+        y_test = test_groundtruths[test_tag_i]
+
+        y_test = to_categorical(y_test)
+
+        # Train/Test split for data
+        print("Training data shape: (X) (y)")
+        print(X_train.shape, y_train.shape)
+        print("Testing data shape: (X) (y)")
+        print(X_test.shape, y_test.shape)
+
+        print('Data loaded! Ready to train')
+
+        # repeat experiment
+        scores = list()
+        for r in range(repeats):
+            score = build_model(X_train, y_train, X_test, y_test)
+            score = score * 100.0
+            print('>#%d: %.3f' % (r+1, score))
+            scores.append(score)
+        # summarize results
+        m = summarize_results(scores)
+        accuracies.append(m)
+
+    print(f"OVERALL ACCURACY WAS {mean(accuracies)}")
+
+
+run_wv_exp()
